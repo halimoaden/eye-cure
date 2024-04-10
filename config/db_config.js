0a3e4a6984fa962d099c3084d74ae8016a4803b4
halimoaden <mamohalimaaden@gmail.com>
2024-04-10 11:56:54 +0300

    database credentials correction

--- config/db_config.js
+++ config/db_config.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const {Sequelize, DataTypes} = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    dialect: 'mysql'
+    dialect: 'mysql',
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST
 });
 
 const dbConn = async () => {
